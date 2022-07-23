546b2b25c1ce58fb006fa4b5a2d1725a302b6e79
Ronald A. Richardson <me@ron.dev>
2022-07-23 16:57:13 +0800

    working and testing ios version

--- ios/VisionCameraObjectScanner.swift
+++ ios/VisionCameraObjectScanner.swift
@@ -1,23 +1,31 @@
 import Vision
+import MLKitObjectDetection
 import MLKitVision
 import CoreML
 
 @objc(ObjectScannerPlugin)
 public class ObjectScannerPlugin: NSObject, FrameProcessorPluginBase {
+  static var ObjectDetectorOption: ObjectDetectorOptions = {
+    let options = ObjectDetectorOptions()
+    // options.detectorMode = .singleImage
+    options.shouldEnableMultipleObjects = true
+    options.shouldEnableClassification = true
+    return options
+  }
+
+  static var objectDetector = ObjectDetector.objectDetector(options: ObjectDetectorOption)
+
   @objc public static func callback(_ frame: Frame!, withArgs _: [Any]!) -> Any! {
-    let buffer = frame.buffer
-    let orientation = frame.orientation
-    // // get image from frame buffer
-    // let image = VisionImage(buffer: buffer)
-    // // prepare mlkit vision options
-    // let options = ObjectDetectorOptions()
-    // // options.detectorMode = .singleImage
-    // options.shouldEnableMultipleObjects = true
-    // options.shouldEnableClassification = true
-    // create object detector instance w/ options
-    // let objectDetector = ObjectDetector.objectDetector(options: options)
-    // get object results
-    // return objectDetector.results(in: image)
-    return []
+    // get image from frame buffer
+    let image = VisionImage(buffer: frame.buffer)
+
+    // get detected objects from image
+    do {
+      let detectedObjects: [Object] = try objectDetector.results(in: image)
+    } catch _ {
+      return nil
+    }
+
+    return detectedObjects
   }
 }
